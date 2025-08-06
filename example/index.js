ccddd33aa0ff463e3e4f4febe992f24a8a59a8dd
Ionică Bizău <bizauionica@gmail.com>
2025-08-06 15:26:53 +0200

    :arrow_up: 1.1.2 :tada:

--- example/index.js
+++ example/index.js
@@ -8,3 +8,13 @@ cliConfetti({}, function (err, c) {
     if (err) throw err;
     CliUpdate.render(c);
 });
+
+
+// ES6 Version
+import cliConfetti from "cli-confetti";
+import CliUpdate from "cli-update";
+
+cliConfetti({}, function (error, confetti) {
+  if (error) throw error;
+  CliUpdate.render(confetti);
+});
